fb4c8adde36ccf045eb1f83e5bf83958e85219b1
sandy <i.am.saha.sandip@gmail.com>
2017-11-24 22:14:23 +0530

    Initial Commit

---
+++ pc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pc
+//
+//  Created by Sandip Saha on 24/11/17.
+//  Copyright © 2017 cnufed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
